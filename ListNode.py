4e5661fbeebc7b0673e2f5308a065ca08b51091d
siyizhou <48543747+siyizhou@users.noreply.github.com>
2019-04-19 22:03:42 +0800

    Update and rename train.py to ListNode.py

---
+++ ListNode.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+a=ListNode(3)
+a.val
+
+a.val=4
+a.val
+
+a.next=ListNode(4)
+b=a.next
+b.val
+
+b.next is None
+b.next==None
