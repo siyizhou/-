79f085e7305c854caafdf77c248bc8b43dc2398c
siyizhou <48543747+siyizhou@users.noreply.github.com>
2019-04-20 10:28:54 +0800

    Update 从尾到头打印链表.py

--- 从尾到头打印链表.py
+++ 从尾到头打印链表.py
@@ -10,3 +10,11 @@ class Solution:
     # 返回从尾部到头部的列表值序列，例如[1,2,3]
     def printListFromTailToHead(self, listNode):
         # write code here
+        l=[]
+        p=listNode
+        while p:
+            l.insert(0,p.val)
+            p=p.next
+        return l
+
+    
